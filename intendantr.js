e22348c59e780a2cfc1db1f9a736518c58c41b9d
hanshenrik <hh.gronsleth@gmail.com>
2015-10-13 00:07:24 +0200

    Merge liquors and mixers to ingredients, lots of other stuff

--- intendantr.js
+++ intendantr.js
@@ -1,12 +1,11 @@
-Liquors = new Mongo.Collection("liquors");
-Mixers = new Mongo.Collection("mixers");
-// Drinks = new Mongo.Collection("drinks");
+Ingredients = new Mongo.Collection("ingredients");
+Drinks = new Mongo.Collection("drinks");
 
 if (Meteor.isServer) {
   // This code only runs on the server
   // Only publish drinks that are public or belong to the current user
-  Meteor.publish("liquors", function () {
-    return Liquors.find({
+  Meteor.publish("ingredients", function () {
+    return Ingredients.find({
       $or: [
         { private: {$ne: true} },
         { owner: this.userId }
@@ -14,83 +13,66 @@ if (Meteor.isServer) {
     });
   });
 
-  Meteor.publish("mixers", function () {
-    return Mixers.find({
+  Meteor.publish("drinks", function () {
+    return Drinks.find({
       $or: [
         { private: {$ne: true} },
         { owner: this.userId }
       ]
     });
   });
-
-  // Meteor.publish("drinks", function () {
-  //   return Drinks.find({
-  //     $or: [
-  //       { private: {$ne: true} },
-  //       { owner: this.userId }
-  //     ]
-  //   });
-  // });
 }
 
 if (Meteor.isClient) {
   // This code only runs on the client
-  Meteor.subscribe("liquors");
-  Meteor.subscribe("mixers");
-  // Meteor.subscribe("drinks");
+  Meteor.subscribe("ingredients");
+  Meteor.subscribe("drinks");
+
+  Template.registerHelper('equals',
+    function(v1, v2) {
+      return (v1 === v2);
+    }
+  );
 
   Template.body.helpers({
-    liquors: function () {
-      if (Session.get("hideUnavailableLiquors")) {
-        // If hide unavailable drinks is checked, filter liquors
-        return Liquors.find({checked: {$ne: true}}, {sort: {name: 1}});
+    ingredients: function () {
+      if (Session.get("hideUnavailableIngredients")) {
+        // If hide unavailable drinks is checked, filter ingredients
+        return Ingredients.find({checked: {$ne: true}}, {sort: {name: 1}});
       } else {
-        // Otherwise, return all of the liquors
-        return Liquors.find({}, {sort: {name: 1}});
+        // Otherwise, return all of the ingredients
+        return Ingredients.find({}, {sort: {name: 1}});
       }
     },
-    hideUnavailableLiquors: function () {
-      return Session.get("hideUnavailableLiquors");
+    hideUnavailableIngredients: function () {
+      return Session.get("hideUnavailableIngredients");
     },
-    availableLiquorsCount: function () {
-      return Liquors.find({checked: {$ne: true}}).count();
+    availableIngredientsCount: function () {
+      return Ingredients.find({checked: {$ne: true}}).count();
+    },
+    totalIngredientsCount: function () {
+      return Ingredients.find({}).count();
     },
 
-    mixers: function () {
-      if (Session.get("hideUnavailableMixers")) {
-        // If hide unavailable drinks is checked, filter mixers
-        return Mixers.find({checked: {$ne: true}}, {sort: {name: 1}});
+    drinks: function () {
+      if (Session.get("hideUnavailableDrinks")) {
+        // If hide unavailable drinks is checked, filter drinks
+        return Drinks.find({checked: {$ne: true}}, {sort: {name: 1}});
       } else {
-        // Otherwise, return all of the mixers
-        return Mixers.find({}, {sort: {name: 1}});
+        // Otherwise, return all of the drinks
+        return Drinks.find({}, {sort: {name: 1}});
       }
     },
-    hideUnavailableMixers: function () {
-      return Session.get("hideUnavailableMixers");
+    hideUnavailableDrinks: function () {
+      return Session.get("hideUnavailableDrinks");
     },
-    availableMixersCount: function () {
-      return Mixers.find({checked: {$ne: true}}).count();
-    },
-
-    // drinks: function () {
-    //   if (Session.get("hideUnavailableDrinks")) {
-    //     // If hide unavailable drinks is checked, filter drinks
-    //     return Drinks.find({checked: {$ne: true}}, {sort: {name: 1}});
-    //   } else {
-    //     // Otherwise, return all of the drinks
-    //     return Drinks.find({}, {sort: {name: 1}});
-    //   }
-    // },
-    // hideUnavailableDrinks: function () {
-    //   return Session.get("hideUnavailableDrinks");
-    // },
-    // availableDrinksCount: function () {
-    //   return Drinks.find({checked: {$ne: true}}).count();
-    // }
+    availableDrinksCount: function () {
+      return Drinks.find({checked: {$ne: true}}).count();
+    }
   });
 
   Template.body.events({
-    "submit .new-liquor": function (event) {
+    "submit .new-ingredient": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
 
@@ -98,106 +80,121 @@ if (Meteor.isClient) {
       var text = event.target.text.value;
 
       // Insert a drink into the collection
-      Meteor.call("addLiquor", text);
+      Meteor.call("addIngredient", text);
 
       // Clear form
       event.target.text.value = "";
     },
-    "change .hide-unavailable-liquors input": function (event) {
-      Session.set("hideUnavailableLiquors", event.target.checked);
+    "change .hide-unavailable-ingredients input": function (event) {
+      Session.set("hideUnavailableIngredients", event.target.checked);
     },
 
-    "submit .new-mixer": function (event) {
+    "submit .new-drink": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
 
+      var ingredientsIds = Ingredients.find(
+        { selected: true },
+        { _id: 1 }
+        // TODO: Could have added name for readability
+      ).map( function(item) { return item._id; });
+      console.log(ingredientsIds)
       // Get value from form element
-      var text = event.target.text.value;
+      var name = event.target.text.value;
 
       // Insert a drink into the collection
-      Meteor.call("addMixer", text);
+      Meteor.call("addDrink", name, ingredientsIds);
 
       // Clear form
       event.target.text.value = "";
+
+      // Clear selected
+      Meteor.call("clearSelectedIngredients");
     },
-    "change .hide-unavailable-mixers input": function (event) {
-      Session.set("hideUnavailableMixers", event.target.checked);
+    "change .hide-unavailable-drinks input": function (event) {
+      // Session.set("hideUnavailableDrinks", event.target.checked);
+    }
+  });
+
+  Template.ingredient.helpers({
+    isOwner: function () {
+      return this.owner === Meteor.userId();
+    }
+  });
+
+  Template.ingredient.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Meteor.call("setCheckedIngredient", this._id, ! this.checked);
     },
-
-    // "submit .new-drink": function (event) {
-    //   // Prevent default browser form submit
-    //   event.preventDefault();
-
-    //   // Get value from form element
-    //   var text = event.target.text.value;
-
-    //   // Insert a drink into the collection
-    //   Meteor.call("addDrink", text);
-
-    //   // Clear form
-    //   event.target.text.value = "";
-    // },
-    // "change .hide-unavailable-drinks input": function (event) {
-    //   Session.set("hideUnavailableDrinks", event.target.checked);
+    "click .delete": function () {
+      Meteor.call("deleteIngredient", this._id);
+    },
+    // "click .toggle-private": function () {
+    //   Meteor.call("setPrivateLiquor", this._id, ! this.private);
     // }
   });
 
-  Template.liquor.helpers({
+  Template.newDrinkIngredient.events({
+    "click .toggle-selected": function () {
+      // Set the checked property to the opposite of its current value
+      Meteor.call("setSelectedIngredient", this._id, ! this.selected);
+    }
+  });
+
+  Template.drink.helpers({
     isOwner: function () {
       return this.owner === Meteor.userId();
+    },
+    getIngredients: function () {
+      // TODO: should probably be done in a nicer way!
+      // Could be made global method and take drinkId as parameter
+      var drink = Drinks.find(
+        { _id: this._id },
+        { fields: {ingredients: 1} }
+      ).fetch()
+
+      var ingredientsIds = drink[0].ingredients
+
+      var ingredients = Ingredients.find(
+        { _id: { $in: ingredientsIds } },
+        { fields: {name: 1, checked: 1} }
+      )
+
+      return ingredients
+    },
+    isAvailable: function () {
+      // TODO: find a way to not duplicate this code!
+      var drink = Drinks.find(
+        { _id: this._id },
+        { fields: {ingredients: 1} }
+      ).fetch()
+
+      var ingredientsIds = drink[0].ingredients
+
+      var unavailableIngredients = Ingredients.find(
+        {
+          _id: { $in: ingredientsIds },
+          checked: true
+        }
+      ).count()
+
+      return unavailableIngredients > 0
     }
   });
 
-  Template.liquor.events({
-    "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Meteor.call("setCheckedLiquor", this._id, ! this.checked);
-    },
+  Template.drink.events({
+    // "click .toggle-checked": function () {
+    //   // Set the checked property to the opposite of its current value
+    //   Meteor.call("setChecked", this._id, ! this.checked);
+    // },
     "click .delete": function () {
-      Meteor.call("deleteLiquor", this._id);
+      Meteor.call("deleteDrink", this._id);
     },
-    "click .toggle-private": function () {
-      Meteor.call("setPrivateLiquor", this._id, ! this.private);
-    }
+    // "click .toggle-private": function () {
+    //   Meteor.call("setPrivate", this._id, ! this.private);
+    // }
   });
-
-  Template.mixer.helpers({
-    isOwner: function () {
-      return this.owner === Meteor.userId();
-    }
-  });
-
-  Template.mixer.events({
-    "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Meteor.call("setCheckedMixer", this._id, ! this.checked);
-    },
-    "click .delete": function () {
-      Meteor.call("deleteMixer", this._id);
-    },
-    "click .toggle-private": function () {
-      Meteor.call("setPrivateMixer", this._id, ! this.private);
-    }
-  });
-
-  // Template.drink.helpers({
-  //   isOwner: function () {
-  //     return this.owner === Meteor.userId();
-  //   }
-  // });
-
-  // Template.drink.events({
-  //   "click .toggle-checked": function () {
-  //     // Set the checked property to the opposite of its current value
-  //     Meteor.call("setChecked", this._id, ! this.checked);
-  //   },
-  //   "click .delete": function () {
-  //     Meteor.call("deleteDrink", this._id);
-  //   },
-  //   "click .toggle-private": function () {
-  //     Meteor.call("setPrivate", this._id, ! this.private);
-  //   }
-  // });
 
   // Accounts.ui.config({
   //   passwordSignupFields: "USERNAME_ONLY"
@@ -205,7 +202,10 @@ if (Meteor.isClient) {
 }
 
 Meteor.methods({
-  addLiquor: function (name) {
+  addIngredient: function (name) {
+    // TODO: make input parameter
+    var type = 'booze'
+
     // Make sure the user is logged in before inserting a drink
     if (! Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
@@ -217,121 +217,81 @@ Meteor.methods({
       // TODO: show error message
     }
 
-    Liquors.insert({
+    Ingredients.insert({
       name: name,
+      type: type,
+      checked: false,
       createdAt: new Date(),
       owner: Meteor.userId(),
       username: Meteor.user().username
     });
   },
-  deleteLiquor: function (liquorId) {
-    var liquor = Liquors.findOne(liquorId);
+  deleteIngredient: function (ingredientId) {
+    var ingredient = Ingredients.findOne(ingredientId);
 
-    if (liquor.owner !== Meteor.userId()) {
+    if (ingredient.owner !== Meteor.userId()) {
       // Make sure only the owner can delete it
       throw new Meteor.Error("not-authorized");
     }
 
-    Liquors.remove(liquorId);
+    Ingredients.remove(ingredientId);
   },
-  setCheckedLiquor: function (liquorId, setCheckedLiquor) {
-    var liquor = Liquors.findOne(liquorId);
+  setCheckedIngredient: function (ingredientId, setCheckedIngredient) {
+    var ingredient = Ingredients.findOne(ingredientId);
 
-    if (liquor.private && liquor.owner !== Meteor.userId()) {
-      // If the liquor is private, make sure only the owner can check it off
+    if (ingredient.private && ingredient.owner !== Meteor.userId()) {
+      // If the ingredient is private, make sure only the owner can check it off
       throw new Meteor.Error("not-authorized");
     }
 
-    Liquors.update(liquorId, { $set: { checked: setCheckedLiquor} });
+    Ingredients.update(ingredientId, { $set: { checked: setCheckedIngredient} });
   },
-  setPrivateLiquor: function (liquorId, setToPrivate) {
-    var liquor = Liquors.findOne(liquorId);
 
-    // Make sure only the liquor owner can make a liquor private
-    if (liquor.owner !== Meteor.userId()) {
+  setSelectedIngredient: function (ingredientId, setSelectedIngredient) {
+    var ingredient = Ingredients.findOne(ingredientId);
+
+    if (ingredient.private && ingredient.owner !== Meteor.userId()) {
+      // If the ingredient is private, make sure only the owner can check it off
       throw new Meteor.Error("not-authorized");
     }
 
-    Liquors.update(liquorId, { $set: { private: setToPrivate } });
+    Ingredients.update(ingredientId, { $set: { selected: setSelectedIngredient} });
+  },
+  clearSelectedIngredients: function () {
+    Ingredients.update(
+      {}, // Select all documents
+      { $set: { selected: false } },
+      { multi: true } // Enable updating multiple documents
+    );
   },
 
-  addMixer: function (name) {
+  addDrink: function (name, ingredientsIds) {
     // Make sure the user is logged in before inserting a drink
     if (! Meteor.userId()) {
       throw new Meteor.Error("not-authorized");
     }
 
-    // Don't allow empty name
     if (name === '') {
       throw new Meteor.Error("empty-input");
       // TODO: show error message
     }
 
-    Mixers.insert({
+    Drinks.insert({
       name: name,
+      ingredients: ingredientsIds,
       createdAt: new Date(),
       owner: Meteor.userId(),
       username: Meteor.user().username
     });
   },
-  deleteMixer: function (mixerId) {
-    var mixer = Mixers.findOne(mixerId);
+  deleteDrink: function (drinkId) {
+    var drink = Drinks.findOne(drinkId);
 
-    if (mixer.owner !== Meteor.userId()) {
+    if (drink.owner !== Meteor.userId()) {
       // Make sure only the owner can delete it
       throw new Meteor.Error("not-authorized");
     }
 
-    Mixers.remove(mixerId);
-  },
-  setCheckedMixer: function (mixerId, setCheckedMixer) {
-    var mixer = Mixers.findOne(mixerId);
-
-    if (mixer.private && mixer.owner !== Meteor.userId()) {
-      // If the mixer is private, make sure only the owner can check it off
-      throw new Meteor.Error("not-authorized");
-    }
-
-    Mixers.update(mixerId, { $set: { checked: setCheckedMixer} });
-  },
-  setPrivateMixer: function (mixerId, setToPrivate) {
-    var mixer = Mixers.findOne(mixerId);
-
-    // Make sure only the mixer owner can make a mixer private
-    if (mixer.owner !== Meteor.userId()) {
-      throw new Meteor.Error("not-authorized");
-    }
-
-    Mixers.update(mixerId, { $set: { private: setToPrivate } });
-  },
-
-  // addDrink: function (name) {
-  //   // MOCK
-  //   var spirits = [ {name: 'Vodka', quantity: '5cl'}, {name: 'Kaluha', quantity: '2cl'} ]
-  //   var mixers = [ {name: 'Fløte', quantity: 'topp-opp'} ]
-
-  //   // Make sure the user is logged in before inserting a drink
-  //   if (! Meteor.userId()) {
-  //     throw new Meteor.Error("not-authorized");
-  //   }
-
-  //   Drinks.insert({
-  //     name: name,
-  //     spirits: spirits,
-  //     mixers: mixers,
-  //     createdAt: new Date(),
-  //     owner: Meteor.userId(),
-  //     username: Meteor.user().username
-  //   });
-  // },
-  // deleteDrink: function (drinkId) {
-  //   var drink = Drinks.findOne(drinkId);
-
-  //   if (drink.owner !== Meteor.userId()) {
-  //     // Make sure only the owner can delete it
-  //     throw new Meteor.Error("not-authorized");
-  //   }
-
-  //   Drinks.remove(drinkId);
-  // }
+    Drinks.remove(drinkId);
+  }
 });
